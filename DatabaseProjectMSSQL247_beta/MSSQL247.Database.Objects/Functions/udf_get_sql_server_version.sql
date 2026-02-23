e4f5aaed59005048e20d0598967513dc458bb018
John Paul Del Mundo <81340844+jpnotsodev@users.noreply.github.com>
2026-02-23 12:21:59 +0800

    add function for getting sql server version

---
+++ DatabaseProjectMSSQL247_beta/MSSQL247.Database.Objects/Functions/udf_get_sql_server_version.sql
@@ -0,0 +1,32 @@
+CREATE FUNCTION dbo.ufn_sqlowl_get_sqlserver_version (
+	@productVersion VARCHAR(20) = NULL)
+RETURNS VARCHAR(155)
+BEGIN 
+	IF @productVersion IS NULL 
+		SET @productVersion = (SELECT CONVERT(NVARCHAR(155), SERVERPROPERTY('productVersion')));
+	RETURN 
+		(SELECT CASE WHEN @productVersion LIKE N'16%' 
+							THEN N'SQL Server 2022'
+				WHEN @productVersion LIKE N'15%' 
+							THEN N'SQL Server 2019' 
+				WHEN @productVersion LIKE N'14%' 
+							THEN N'SQL Server 2017' 
+				WHEN @productVersion LIKE N'13%' 
+							THEN N'SQL Server 2016' 
+				WHEN @productVersion LIKE N'12%' 
+							THEN N'SQL Server 2015' 
+				WHEN @productVersion LIKE N'11%' 
+							THEN N'SQL Server 2012' 
+				WHEN @productVersion LIKE N'10.5%' 
+							THEN N'SQL Server 2008 R2' 
+				WHEN @productVersion LIKE N'10%' 
+							THEN N'SQL Server 2008'  
+				WHEN @productVersion LIKE N'9%' 
+							THEN N'SQL Server 2005'  
+				WHEN @productVersion LIKE N'8%' 
+							THEN N'SQL Server 2000'  
+				WHEN @productVersion LIKE N'7%' 
+							THEN N'SQL Server 7.0'  
+				WHEN @productVersion LIKE N'6.50%' 
+							THEN N'SQL Server 6.5' END)
+END
