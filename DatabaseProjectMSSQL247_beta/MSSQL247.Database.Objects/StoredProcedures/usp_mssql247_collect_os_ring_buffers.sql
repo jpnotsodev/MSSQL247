1e828708ddfc9b0189a31c3fb555d9f2aea4d0e4
John Paul Del Mundo <81340844+jpnotsodev@users.noreply.github.com>
2026-02-27 16:23:39 +0800

    add ring buffer collector

--- DatabaseProjectMSSQL247_beta/MSSQL247.Database.Objects/StoredProcedures/usp_mssql247_collect_os_ring_buffers.sql
+++ DatabaseProjectMSSQL247_beta/MSSQL247.Database.Objects/StoredProcedures/usp_mssql247_collect_os_ring_buffers.sql
@@ -1,12 +1,30 @@
 CREATE PROCEDURE [dbo].[usp_mssql247_collect_os_ring_buffers]
 AS
-  WITH ring_buffers_filtered AS (
-    SELECT ring_buffer_address, 
+  
+  WITH ring_buffer_resource_last_record_ids (ring_buffer_type, record_id) AS (
+    SELECT 
+      ring_buffer_type, 
+      MAX(record.value('(/Record/@id)[1]', 'int')) record_id 
+    FROM (
+          SELECT timestamp, CAST(record AS XML) record, ring_buffer_type FROM sys.dm_os_ring_buffers
+            WHERE ring_buffer_type = 'RING_BUFFER_RESOURCE_MONITOR'
+              OR ring_buffer_type = 'RING_BUFFER_SCHEDULER_MONITOR'
+          ) ringbuffer
+    GROUP BY ring_buffer_type
+  ), ring_buffer AS (
+    SELECT *, record.value('(/Record/@id)[1]', 'int') record_id
+    FROM (
+          SELECT 
+            ring_buffer_address, 
             ring_buffer_type, 
-            DATEADD(MS, timestamp - (SELECT ms_ticks FROM sys.dm_os_sys_info), GETDATE()) timestamp, 
-            CAST(record AS XML) record FROM sys.dm_os_ring_buffers 
-      WHERE ring_buffer_type IN ('RING_BUFFFER_RESOUCE_MONITOR', 
-                                    'RING_BUFFER_SCHEDULER_MONITOR')
+            timestamp, 
+            CAST(record AS XML) record 
+          FROM sys.dm_os_ring_buffers 
+          WHERE ring_buffer_type = 'RING_BUFFER_RESOURCE_MONITOR'
+              OR ring_buffer_type = 'RING_BUFFER_SCHEDULER_MONITOR'
+      ) annon1
+    WHERE record.value('(/Record/@id)[1]', 'int') IN (
+            SELECT record_id FROM ring_buffer_resource_last_record_ids)
   )
     INSERT mssql247_os_ring_buffers 
       (sql_instance,
@@ -14,10 +32,11 @@ AS
         ring_buffer_type,
         ring_buffer_timestamp,
         record)
-      SELECT dbo.udf_get_server_name(),
+      SELECT 
+        dbo.udf_get_server_name(),
         ring_buffer_address,
         ring_buffer_type,
-        timestamp,
+        DATEADD(MS, timestamp - (SELECT ms_ticks FROM sys.dm_os_sys_info), getdate()),
         CONVERT(NVARCHAR(2048), record)
-      FROM ring_buffers_filtered
+      FROM ring_buffer
 RETURN 0
--- DatabaseProjectMSSQL247_beta/MSSQL247.Database.Objects/Tables/mssql247_os_ring_buffers.sql
+++ DatabaseProjectMSSQL247_beta/MSSQL247.Database.Objects/Tables/mssql247_os_ring_buffers.sql
@@ -6,5 +6,8 @@ CREATE TABLE [dbo].[mssql247_os_ring_buffers]
   [ring_buffer_type] VARCHAR(155) NOT NULL,
   [ring_buffer_timestamp] DATETIME,
   [record] VARCHAR(2048) NOT NULL,
-  [snapshot_datetime] DATETIME DEFAULT GETDATE()
+  [snapshot_timestamp] DATETIME
 )
+
+ALTER TABLE [dbo].[mssql247_os_ring_buffers] 
+ADD CONSTRAINT df_os_ring_buffer_napshot_timestamp_default_getdate DEFAULT GETDATE() FOR [snapshot_timestamp]
